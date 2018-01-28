008521165a0e53ba8865f4c79afec35f5c2d520b
Pawel Kozicki <pe.kozicki@gmail.com>
2018-01-28 17:38:10 +0100

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import TextField from 'material-ui/TextField';
+import DatePicker from 'material-ui/DatePicker';
+import RaisedButton from 'material-ui/RaisedButton';
+
+const containerStyle = {
+    width: '50%'    
+};
+
+const style = {    
+    margin: 12
+};
+
+
+export default class App extends React.Component {
+    render() {
+        return(
+            <div style={ containerStyle }>
+                <MuiThemeProvider>
+                    <AppBar
+                        title="Reminder Pro"
+                        iconClassNameRight="muidocs-icon-navigation-expand-more"
+                        style={ style }
+                    />
+                    <br />
+                    <TextField
+                        hintText="I have to..."
+                        floatingLabelText="New Task"
+                        floatingLabelFixed={ true }
+                        style={ style }
+                    />
+                    <br />
+                    <DatePicker
+                        hintText="Set due date"
+                        container="inline"
+                        style={ style }
+                    />
+                    <br />
+                    <RaisedButton label="Add reminder" primary={ true } style={ style } />
+                </MuiThemeProvider>
+            </div>
+            
+        )
+    }
+}
